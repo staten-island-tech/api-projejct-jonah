a6a38ba65eeb590cac3a4f5268aba85dd47b65b5
wreckingwall999 <113192884+wreckingwall999@users.noreply.github.com>
2023-01-12 10:24:58 -0500

    Update __init__.py

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -97,25 +97,25 @@ def create_app(test_config=None):
         else:
             return render_template('events.html',data=data1["data"]["results"])
     @app.route("/series", methods=('GET', 'POST'))
-    def event():
+    def series():
         if request.method == "GET":
             return render_template("ask.html")
         else:
             param1=request.form["title"]
-            param="/events/"+param1
+            param="/series/"+param1
             data1=request1(param)
             if data1==0:
                 return render_template("error.html")
             else:
-                return render_template('event.html',character=data1["data"]["results"][0])
+                return render_template('series.html',character=data1["data"]["results"][0])
     @app.route("/serieses")
-    def events():
+    def serieses():
         param="/series"
         data1=request1(param)
         if data1==0:
             return render_template("error.html")
         else:
-            return render_template('events.html',data=data1["data"]["results"])
+            return render_template('serieses.html',data=data1["data"]["results"])
     @app.route("/event", methods=('GET', 'POST'))
     def event():
         if request.method == "GET":
